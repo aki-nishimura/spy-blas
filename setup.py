13765458153a4c04b123d1f435e67bbe0db36ed7
Aki Nishimura <akihiko4@g.ucla.edu>
2019-02-28 13:36:58 -0800

    Remove (apparently) unnecessary addition of the anaconda include directory to the header files search path.

--- setup.py
+++ setup.py
@@ -1,18 +1,7 @@
 from distutils.core import setup, Extension
 from Cython.Build import cythonize
 import numpy as np
-import os
 
-
-anaconda_path = "~/anaconda3"
-help_pagename = "Finding your Anaconda Python interpreter path"
-help_url = "https://docs.anaconda.com/anaconda/user-guide/tasks/integration/python-path/"
-if not os.path.exists(os.path.expanduser(anaconda_path)):
-    raise FileNotFoundError(
-        "Could not find Anaconda installation in the default location. "
-        "Manually set the path within the setup script. "
-        "(Ref. '{pagename}': {url}).".format(pagename=help_pagename, url=help_url)
-    )
 
 ext_modules = [
     Extension(
@@ -21,7 +10,6 @@ ext_modules = [
             "mkl_spblas.pyx"
         ],
         include_dirs=[
-            anaconda_path + "/include",
             np.get_include(),
         ],
     )
