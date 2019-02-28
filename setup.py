df9cafe3bbfbc1b890e3dd480f97ba92f3f1a849
Aki Nishimura <akihiko4@g.ucla.edu>
2019-02-28 11:34:30 -0800

    Raise an error when the default path to Anaconda may not be correct.

--- setup.py
+++ setup.py
@@ -1,9 +1,18 @@
 from distutils.core import setup, Extension
 from Cython.Build import cythonize
 import numpy as np
+import os
 
 
 anaconda_path = "~/anaconda3"
+help_pagename = "Finding your Anaconda Python interpreter path"
+help_url = "https://docs.anaconda.com/anaconda/user-guide/tasks/integration/python-path/"
+if not os.path.exists(os.path.expanduser(anaconda_path)):
+    raise FileNotFoundError(
+        "Could not find Anaconda installation in the default location. "
+        "Manually set the path within the setup script. "
+        "(Ref. '{pagename}': {url}).".format(pagename=help_pagename, url=help_url)
+    )
 
 ext_modules = [
     Extension(
